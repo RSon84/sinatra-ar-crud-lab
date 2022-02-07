22939dfa568ea04d7d075778c0c4d20d6ee7b66b
devin <craigvin84@yahoo.com>
2022-02-06 17:55:43 -0700

    working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,42 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    @article = article.create(params)
+    redirect to '/articles'
+  end
+
+  get '/articles' do
+    @articles = Post.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = article.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = article.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = article.find_by_id(params[:id])
+    @article.name = params[:name]
+    @article.content = params[:content]
+    @article.save
+    erb :show
+  end
+
+
+  delete '/articles/:id/delete' do
+    @article2 = article.find_by_id(params[:id])
+    @article2.delete
+    erb :delete
   end
 end
