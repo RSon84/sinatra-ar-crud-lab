14904f63cb47e2b85d93cf2432e2856a4bc967d6
devin <craigvin84@yahoo.com>
2022-02-09 21:25:54 -0700

    commit tree 6e2b2b185950cd4b422d0992b972597a4a628e92

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,42 +8,47 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+# get '/' do
+#   redirect to "/articles"
+# end
+
   get '/articles/new' do
+    @article=Article.new
     erb :new
   end
 
-  post '/articles' do
+   post '/articles' do
     @article = article.create(params)
-    redirect to '/articles'
+    redirect to "/articles/#{@article.id}"
   end
 
-  get '/articles' do
-    @articles = Post.all
-    erb :index
-  end
+  # get '/articles' do
+  #   @article = Post.all
+  #   erb :index
+  # end
 
-  get '/articles/:id' do
-    @article = article.find_by_id(params[:id])
-    erb :show
-  end
+#   get '/articles/:id' do
+#     @article = article.find_by_id(params[:id])
+#     erb :show
+#   end
 
-  get '/articles/:id/edit' do
-    @article = article.find_by_id(params[:id])
-    erb :edit
-  end
+#   get '/articles/:id/edit' do
+#     @article = article.find_by_id(params[:id])
+#     erb :edit
+#   end
 
-  patch '/articles/:id' do
-    @article = article.find_by_id(params[:id])
-    @article.name = params[:name]
-    @article.content = params[:content]
-    @article.save
-    erb :show
-  end
+#   patch '/articles/:id' do
+#     @article = article.find_by_id(params[:id])
+#     @article.name = params[:name]
+#     @article.content = params[:content]
+#     @article.save
+#     erb :show
+#   end
 
 
-  delete '/articles/:id/delete' do
-    @article2 = article.find_by_id(params[:id])
-    @article2.delete
-    erb :delete
-  end
+#   delete '/articles/:id/delete' do
+#     @article2 = article.find_by_id(params[:id])
+#     @article2.delete
+#     erb :delete
+#   end
 end
